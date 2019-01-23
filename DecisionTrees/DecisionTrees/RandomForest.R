f8169dcb1fc44531478a6563c2ca9e23bb1574ba
Thomas Angeland <thomas.angeland@gmail.com>
2019-01-23 23:17:14 +0100

    Updated random forest training results.

--- DecisionTrees/DecisionTrees/RandomForest.R
+++ DecisionTrees/DecisionTrees/RandomForest.R
@@ -17,8 +17,8 @@ dataset <- as.data.frame(flag.data)
 dataset$religion <- as.factor(dataset$religion)
 
 # Pre-processing
-trans <- preProcess(dataset, method = c("BoxCox", "center", "scale", "pca"))
-dataset <- predict(trans, dataset)
+#trans <- preProcess(dataset, method = c("BoxCox", "center", "scale", "pca"))
+#dataset <- predict(trans, dataset)
 
 # Create model with default paramters and auto mtry tuning
 control <- trainControl(method = "repeatedcv", number = 10, repeats = 3, search = "random")
@@ -31,11 +31,11 @@ print(rf_default)
 plot(rf_default)
 
 # without pre-processing:
-# mtry = 97
-# Accuracy 0.6782990
-# Kappa 0.5878833
+# mtry = 17
+# Accuracy 0.7057256
+# Kappa 0.6263600
 
 # with pre-processing:
-# mtry = 47
-# Accuracy 0.5311461
-# Kappa 0.3811316
+# mtry = 9
+# Accuracy 0.5260965  
+# Kappa 0.3797415
